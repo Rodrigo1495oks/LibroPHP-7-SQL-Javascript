2041bc2671cbc5ddeec094d6df3f4218122bb726
Rodrigo1495oks <rodrir1495@gmail.com>
2021-11-27 16:57:49 -0300

    inicializacion

---
+++ 3- introduccionPHP/2-sintaxis-y-estructura.php
@@ -0,0 +1,68 @@
+<?php
+
+// 1. comentarios
+
+// 2. punto y coma
+
+$x = 10;
+
+echo `${x}`;
+
+// 3. asignacion de variables
+
+$myCounter = 1;
+$myString = 'Hello';
+$myArray = array('uno', 'dos', 'tres');
+
+echo $myCounter;
+
+// cadenas
+?>
+
+
+<br>
+<?php
+$username = 'Rodrigo';
+echo $username;
+
+?>
+
+
+<!-- variables numericas -->
+<br>
+<?php
+    $count=17.5;
+
+
+?>
+
+<!-- matrices -->
+<br>
+<?php
+
+$team=array('Bill','Mary','Mike','Chris','Anne');
+
+echo $team[3];
+
+?>
+
+
+<?php
+    $oxo=array(
+        array('x',' ','o'),
+        array('o','o','x'),
+        array('x','o',' ')
+    );
+
+    echo($oxo[1][2]);
+?>
+<br>
+<h1>Como nombrar variables</h1>
+<?php
+    
+
+
+
+?>
+
+
---
+++ cgi-bin/phpinfo.cgi
@@ -0,0 +1,2 @@
+#!"F:/Ampps/php74/php-cgi.exe" -q 
+<?php phpinfo(); ?>
