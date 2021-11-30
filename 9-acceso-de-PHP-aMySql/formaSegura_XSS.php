4c3a9eff08d4fdbf52b9effc4e60073d9fa9f05a
Rodrigo1495oks <rodrir1495@gmail.com>
2021-11-30 18:00:19 -0300

    stagede

--- 9-acceso-de-PHP-aMySql/formaSegura_XSS.php
+++ 9-acceso-de-PHP-aMySql/formaSegura_XSS.php
@@ -5,8 +5,8 @@ if ($conn->connect_error) {
     die("<br> Fatal Error");
 }
 
-$user=mysql_fix_string($conn, $_POST['user']);
-$pass=mysql_fix_string($conn, $_POST['pass']);
+$user=mysql_entities_fix_string($conn, $_POST['user']);
+$pass=mysql_entities_fix_string($conn, $_POST['pass']);
 $query="SELECT * FROM users WHERE user='$user' AND pass='$pass'";
 // etc.
 
@@ -25,3 +25,10 @@ function mysql_fix_string($conn, $string){
         return $conn->real_scape_string($string);
     }
 }
+
+function mysql_entities_fix_string($conn,$string){
+    return htmlentities(mysql_fix_string($conn,$string));
+}
+
+
+?>
